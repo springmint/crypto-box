e5b9dacdd39d5826caeced8987943f1939ff8b5c
kutang <18303592607@163.com>
2023-09-07 16:15:44 +0800

    fix test code

--- hardhat/contracts/Token.sol
+++ hardhat/contracts/Token.sol
@@ -3,34 +3,10 @@
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token {
-    string public name = "Hardhat token";
-    string public symbol = "CPBOX";
-
-    uint256 public totalSupply = 10_000_000_000;
-
-    address public owner;
-
-    mapping(address => uint256) balances;
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    constructor() {
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
-    }
-
-    function transfer(address to, uint256 amount) external {
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-
-        emit Transfer(msg.sender, to, amount);
-    }
-
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+contract Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 100_000 * 10 ** 18);
     }
 }
